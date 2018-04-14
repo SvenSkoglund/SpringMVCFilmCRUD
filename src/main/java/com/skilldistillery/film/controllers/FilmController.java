650a26d4a5cfef0b4a8aeabb5cc219d85bde3966
Xian Zheng <xzheng2112@gmail.com>
2018-04-14 09:45:44 -0600

    added getFilmByKeyword :alien:

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.film.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,6 +51,16 @@ public class FilmController {
 		Film film = dao.getFilmById(filmID);
 		mv.setViewName("WEB-INF/result.jsp");
 		mv.addObject("film", dao.getFilmById(film.getId()));
+		return mv;
+	}
+	@RequestMapping(path="filmByKeyWord.do", method=RequestMethod.GET)
+	public ModelAndView findFilmByKeyword(String keyword) {
+		ModelAndView mv = new ModelAndView();
+		List<Film> films = dao.getFilmByKeyword(keyword);
+		mv.setViewName("WEB-INF/result.jsp");
+		for (Film film : films) {
+			mv.addObject("film", dao.getFilmById(film.getId()));
+		}
 		return mv;
 	}
 	
